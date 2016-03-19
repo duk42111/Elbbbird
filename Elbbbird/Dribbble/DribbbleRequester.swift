fdfa4a45bc3e39a968a8d3c66e2c56b6ce7488d1
Andyy Hope <andyyhope@users.noreply.github.com>
2016-03-19 11:16:21 +1100

    UPDATE: Tidied up Requester class

--- Elbbbird/Dribbble/DribbbleRequester.swift
+++ Elbbbird/Dribbble/DribbbleRequester.swift
@@ -12,7 +12,7 @@ import SwiftyJSON
 
 
 extension DribbbleRequester {
-    typealias ShotsCompletionBlock = () -> Void
+    typealias ShotsCompletionBlock = (shots: [Shot]) -> Void
     
     private struct Constant {
         static let accessToken = "a589847521cfb6420457b84d97addee8c7b108ad49d9a5768f66109bc0bbea21"
@@ -21,12 +21,16 @@ extension DribbbleRequester {
 }
 
 class DribbbleRequester {
+    
+    // MARK: - Requester
+    
     class func requestShots(completion: ShotsCompletionBlock) {
         
         let page: String = "1"
         let limit: String = "1"
         
-        let url = "https://api.dribbble.com/v1/shots?" +
+        let url: String =
+            "https://api.dribbble.com/v1/shots?" +
             "access_token=" + Constant.accessToken +
             "&page=" + page +
             "&per_page=" + limit
@@ -37,23 +41,36 @@ class DribbbleRequester {
         .request(.GET, url)
         .responseJSON { (response) -> Void in
             switch response.result {
+            
             case .Success:
-                guard let value = response.result.value else { return }
-                
-                let json = JSON(value)
-                
-                let items = json.arrayValue.map({ Shot(json: $0) })
-                print(items.count)
-                
-                for item in items {
-                    print(item.title!)
-                    
-                }
+                parseShots(forResponse: response, completion: completion)
                 
             case .Failure:
-                print("Error")
+                completion(shots: [])
             }
         }
+    }
+    
+    // MARK: Parse
+    
+    private class func parseShots(forResponse response: Response<AnyObject, NSError>, completion: ShotsCompletionBlock) {
+        guard let value = response.result.value else {
+            completion(shots: [])
+            return
+        }
         
+        let json = JSON(value)
+        
+        let shots: [Shot] = json.arrayValue.map({
+            Shot(json: $0)
+        })
+        
+        shots
+        .forEach({
+            let title = $0.title ?? ""
+            print(title)
+        })
+        
+        completion(shots: shots)
     }
 }
